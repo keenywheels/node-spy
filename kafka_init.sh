d0a7c24c28ed2ea0a6d599b530ab38273ef80392
damedelion <ivan.shishkov7@yandex.ru>
2025-11-12 23:23:05 +0300

    update infra

--- kafka_init.sh
+++ kafka_init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /opt/kafka/bin/
-./kafka-topics.sh --bootstrap-server ${KAFKA_HOST}:9092 --create --topic test-topic
+./kafka-topics.sh --bootstrap-server ${KAFKA_HOST}:9092 --create --topic scraper_data --if-not-exists
